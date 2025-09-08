fd9ac988a99390ede29f43ab7318dad90f976a55
rongjuanzhao <46863366+rongjuanzhao@users.noreply.github.com>
2025-09-08 08:26:04 +0800

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,28 @@
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      ...(process.env.R2_PUBLIC_URL
+        ? [
+            {
+              hostname: process.env.R2_PUBLIC_URL.replace("https://", ""),
+            },
+          ]
+        : []),
+    ],
+  },
+  compiler: {
+    removeConsole:
+      process.env.NODE_ENV === "production"
+        ? {
+            exclude: ["error"],
+          }
+        : false,
+  },
+};
+
+export default withNextIntl(nextConfig);
